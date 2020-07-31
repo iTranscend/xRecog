395ea88791e1c7221427a6755ae6952fbae5e3c0
Miraclx <omiraculous@gmail.com>
2020-07-31 19:24:53 +0100

    Export EventEmitter to its own script

--- ui/__init__.py
+++ ui/__init__.py
@@ -17,27 +17,7 @@ from PyQt5 import (
     QtPrintSupport,
     QtMultimediaWidgets)
 
-
-class EventEmitter(object):
-    def __init__(self):
-        self.handlers = {}
-
-    def on(self, objectName, handler):
-        if (objectName not in self.handlers):
-            self.handlers[objectName] = []
-        self.handlers[objectName].append(handler)
-
-    def emit(self, objectName, *args):
-        if (objectName not in self.handlers):
-            return
-        for handler in self.handlers[objectName]:
-            handler(*args)
-
-    def removeListener(self, objectName, handler):
-        if (objectName not in self.handlers):
-            return
-        if handler in self.handlers[objectName]:
-            self.handlers[objectName].remove(handler)
+from ui.eventemitter import EventEmitter
 
 
 class XrecogCaptureWindow(QtWidgets.QDialog):
