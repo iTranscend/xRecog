81da72d4f23fcd0e7a6e19707c5eea50a033e9db
Miraclx <omiraculous@gmail.com>
2020-08-15 02:18:31 +0100

    Add a base implementation of the main entrypoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from core import extract_embeddings
 from core import recognize_video
 from core import train_model
+import yaml
 import sys
 import os
 from ui import QtWidgets, XrecogMainWindow
@@ -37,18 +38,53 @@ def recognizeVideo():
     recognize_video.init(args)
 
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = XrecogMainWindow()
-    main_window.show()
-    app.exec_()
+def getStudentsFromDatabase():
+    return []
+
+
+def registerStudent(student):
+    print("registerStudent")
+    # extractEmbeddings()
+    # trainModel()
+    main_window.loadStudent(student)
+    # create student folder
+    # save images
+    main_window.resetRegistrationForm()
+
+
+def startCameraButtonClicked(*args):
+    print("startCameraButtonClicked")
+    # recognizeVideo()
+
+
+def stopCameraButtonClicked(*args):
+    print("stopCameraButtonClicked")
+
+
+def mountMainInstance():
+    with open("config.yml") as conf:
+        CONFIG = yaml.safe_load(conf)
+
+    yearObject = CONFIG.get("year", {"min": 2014, "max": 2023})
+    main_window.setRegistrationYearRange(
+        yearObject.get("min"),
+        yearObject.get("max")
+    )
+
+    main_window.loadCourses(CONFIG.get("courses", []))
+    main_window.loadStudents(getStudentsFromDatabase())
+    main_window.setAboutText("APP DESCRIPTION")
+    main_window.on("registrationData", registerStudent)
+    main_window.on("startCameraButtonClicked", startCameraButtonClicked)
+    main_window.on("stopCameraButtonClicked", stopCameraButtonClicked)
 
 
 if __name__ == "__main__":
     if (not os.path.exists("core/output")):
         os.mkdir("core/output")
-
-    extractEmbeddings()
-    trainModel()
-    recognizeVideo()
-    # main()
+    app = QtWidgets.QApplication(sys.argv)
+    global main_window
+    main_window = XrecogMainWindow()
+    main_window.show()
+    mountMainInstance()
+    app.exec_()
