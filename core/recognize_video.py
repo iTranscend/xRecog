a0ad1c0b8fc84c956e90036a1577e91c03533940
Miraclx <omiraculous@gmail.com>
2020-08-19 02:26:04 +0100

    Remove init function from both train_model and recognize_video scripts

--- core/recognize_video.py
+++ core/recognize_video.py
@@ -17,159 +17,155 @@ import os
 import mysql.connector
 from mysql.connector import Error
 
-if __name__ == "__main__":
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--detector", required=True,
-                    help="path to OpenCV's deep learning face detector")
-    ap.add_argument("-m", "--embedding-model", required=True,
-                    help="path to OpenCV's deep learning face embedding model")
-    ap.add_argument("-r", "--recognizer", required=True,
-                    help="path to model trained to recognize faces")
-    ap.add_argument("-l", "--le", required=True,
-                    help="path to label encoder")
-    ap.add_argument("-c", "--confidence", type=float, default=0.5,
-                    help="minimum probability to filter weak detections")
-    args = vars(ap.parse_args())
-    init(args)
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--detector", required=True,
+                help="path to OpenCV's deep learning face detector")
+ap.add_argument("-m", "--embedding-model", required=True,
+                help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-r", "--recognizer", required=True,
+                help="path to model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+                help="path to label encoder")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
 
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+modelPath = os.path.sep.join([args["detector"],
+                              "res10_300x300_ssd_iter_140000.caffemodel"])
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-def init(args):
-    # load our serialized face detector from disk
-    print("[INFO] loading face detector...")
-    protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
-    modelPath = os.path.sep.join([args["detector"],
-                                  "res10_300x300_ssd_iter_140000.caffemodel"])
-    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
 
-    # load our serialized face embedding model from disk
-    print("[INFO] loading face recognizer...")
-    embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+le = pickle.loads(open(args["le"], "rb").read())
 
-    # load the actual face recognition model along with the label encoder
-    recognizer = pickle.loads(open(args["recognizer"], "rb").read())
-    le = pickle.loads(open(args["le"], "rb").read())
+# initialize the video stream, then allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
 
-    # initialize the video stream, then allow the camera sensor to warm up
-    print("[INFO] starting video stream...")
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
+# start the FPS throughput estimator
+fps = FPS().start()
 
-    # start the FPS throughput estimator
-    fps = FPS().start()
+try:
+    connection = mysql.connector.connect(
+        host='localhost', database='attendance', user='root', password='')
+    # loop over frames from the video file stream
+    while True:
+        # grab the frame from the threaded video stream
+        frame = vs.read()
 
-    try:
-        connection = mysql.connector.connect(
-            host='localhost', database='attendance', user='root', password='')
-        # loop over frames from the video file stream
-        while True:
-            # grab the frame from the threaded video stream
-            frame = vs.read()
+        # resize the frame to have a width of 600 pixels (while
+        # maintaining the aspect ratio), and then grab the image
+        # dimensions
+        frame = imutils.resize(frame, width=600)
+        (h, w) = frame.shape[:2]
 
-            # resize the frame to have a width of 600 pixels (while
-            # maintaining the aspect ratio), and then grab the image
-            # dimensions
-            frame = imutils.resize(frame, width=600)
-            (h, w) = frame.shape[:2]
+        # construct a blob from the image
+        imageBlob = cv2.dnn.blobFromImage(
+            cv2.resize(frame, (300, 300)), 1.0, (300, 300),
+            (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-            # construct a blob from the image
-            imageBlob = cv2.dnn.blobFromImage(
-                cv2.resize(frame, (300, 300)), 1.0, (300, 300),
-                (104.0, 177.0, 123.0), swapRB=False, crop=False)
+        # apply OpenCV's deep learning-based face detector to localize
+        # faces in the input image
+        detector.setInput(imageBlob)
+        detections = detector.forward()
 
-            # apply OpenCV's deep learning-based face detector to localize
-            # faces in the input image
-            detector.setInput(imageBlob)
-            detections = detector.forward()
+        # loop over the detections
+        for i in range(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
 
-            # loop over the detections
-            for i in range(0, detections.shape[2]):
-                # extract the confidence (i.e., probability) associated with
-                # the prediction
-                confidence = detections[0, 0, i, 2]
+            # filter out weak detections
+            if confidence > args["confidence"]:
+                # compute the (x, y)-coordinates of the bounding box for
+                # the face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-                # filter out weak detections
-                if confidence > args["confidence"]:
-                    # compute the (x, y)-coordinates of the bounding box for
-                    # the face
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
+                # extract the face ROI
+                face = frame[startY:endY, startX:endX]
+                (fH, fW) = face.shape[:2]
 
-                    # extract the face ROI
-                    face = frame[startY:endY, startX:endX]
-                    (fH, fW) = face.shape[:2]
+                # ensure the face width and height are sufficiently large
+                if fW < 20 or fH < 20:
+                    continue
 
-                    # ensure the face width and height are sufficiently large
-                    if fW < 20 or fH < 20:
-                        continue
+                # construct a blob for the face ROI, then pass the blob
+                # through our face embedding model to obtain the 128-d
+                # quantification of the face
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                                                 (96, 96), (0, 0, 0), swapRB=True, crop=False)
+                embedder.setInput(faceBlob)
+                vec = embedder.forward()
 
-                    # construct a blob for the face ROI, then pass the blob
-                    # through our face embedding model to obtain the 128-d
-                    # quantification of the face
-                    faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-                                                     (96, 96), (0, 0, 0), swapRB=True, crop=False)
-                    embedder.setInput(faceBlob)
-                    vec = embedder.forward()
+                # perform classification to recognize the face
+                preds = recognizer.predict_proba(vec)[0]
+                j = np.argmax(preds)
+                proba = preds[j]
+                name = le.classes_[j]
 
-                    # perform classification to recognize the face
-                    preds = recognizer.predict_proba(vec)[0]
-                    j = np.argmax(preds)
-                    proba = preds[j]
-                    name = le.classes_[j]
+                # draw the bounding box of the face along with the
+                # associated probability
+                text = "{}: {:.2f}%".format(name, proba * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              (0, 0, 255), 2)
+                cv2.putText(frame, text, (startX, y),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 2)
 
-                    # draw the bounding box of the face along with the
-                    # associated probability
-                    text = "{}: {:.2f}%".format(name, proba * 100)
-                    y = startY - 10 if startY - 10 > 10 else startY + 10
-                    cv2.rectangle(frame, (startX, startY), (endX, endY),
-                                  (0, 0, 255), 2)
-                    cv2.putText(frame, text, (startX, y),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 2)
+                print(type(name))
+                print(type(text))
+                # Update the database
 
-                    print(type(name))
-                    print(type(text))
-                    # Update the database
+                # if connection.is_connected():
+                # 	db_Info = connection.get_server_info()
+                # 	print('Connected to server: ', db_Info)
 
-                    # if connection.is_connected():
-                    # 	db_Info = connection.get_server_info()
-                    # 	print('Connected to server: ', db_Info)
+                cursor = connection.cursor(prepared=True)
+                # cursor = connection.cursor()
+                sql = "UPDATE attendees SET is_present = 1 WHERE matric_no LIKE %s;"
+                casted = str(name).strip()
+                val = (casted)
+                cursor.execute(sql, (casted, ))
+                # cursor.execute(sql)
 
-                    cursor = connection.cursor(prepared=True)
-                    # cursor = connection.cursor()
-                    sql = "UPDATE attendees SET is_present = 1 WHERE matric_no LIKE %s;"
-                    casted = str(name).strip()
-                    val = (casted)
-                    cursor.execute(sql, (casted, ))
-                    # cursor.execute(sql)
+                connection.commit()
+                print("DETECTED [%s]" % val)
+                print(type(casted))
 
-                    connection.commit()
-                    print(val)
-                    print(type(casted))
+        # update the FPS counter
+        fps.update()
 
-            # update the FPS counter
-            fps.update()
+        # show the output frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-            # show the output frame
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                break
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
 
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    finally:
-        if (connection.is_connected()):
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-    # stop the timer and display FPS information
-    fps.stop()
-    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-    # do a bit of cleanup
-    cv2.destroyAllWindows()
-    vs.stop()
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
