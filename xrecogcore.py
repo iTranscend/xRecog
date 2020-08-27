84e6fa6936446a222568e73a1a2c4acc036d49f9
Miraclx <omiraculous@gmail.com>
2020-08-27 18:18:47 +0100

    Implement a means to add more images after student creation

--- xrecogcore.py
+++ xrecogcore.py
@@ -65,64 +65,71 @@ class XRecogCore(object):
     def addStudent(self, matricCode, images):
         self._addStudent(matricCode, images, self.processQueue)
 
+    def addImage(self, matricCode, image):
+        print("[INFO] processing image for [{}] {}/{}".format(matricCode))
+        self._addImage(matricCode, image, self.processQueue)
+
     def _addStudent(self, matricCode, images, pQueue):
         for (index, imagePath) in enumerate(images):
             print("[INFO] processing image {}/{}".format(index + 1, len(images)))
-            # load the image, resize it to have a width of 600 pixels (while
-            # maintaining the aspect ratio), and then grab the image
-            # dimensions
-            image = cv2.imread(imagePath)
-            image = imutils.resize(image, width=600)
-            (h, w) = image.shape[:2]
+            self._addImage(matricCode, imagePath, pQueue)
 
-            # construct a blob from the image
-            imageBlob = cv2.dnn.blobFromImage(
-                cv2.resize(image, (300, 300)), 1.0, (300, 300),
-                (104.0, 177.0, 123.0), swapRB=False, crop=False)
+    def _addImage(self, matricCode, imagePath, pQueue):
+        # load the image, resize it to have a width of 600 pixels (while
+        # maintaining the aspect ratio), and then grab the image
+        # dimensions
+        image = cv2.imread(imagePath)
+        image = imutils.resize(image, width=600)
+        (h, w) = image.shape[:2]
 
-            # apply OpenCV's deep learning-based face detector to localize
-            # faces in the input image
-            self.detector.setInput(imageBlob)
-            detections = self.detector.forward()
+        # construct a blob from the image
+        imageBlob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 1.0, (300, 300),
+            (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-            # ensure at least one face was found
-            if len(detections) > 0:
-                # we're making the assumption that each image has only ONE
-                # face, so find the bounding box with the largest probability
-                i = np.argmax(detections[0, 0, :, 2])
-                confidence = detections[0, 0, i, 2]
+        # apply OpenCV's deep learning-based face detector to localize
+        # faces in the input image
+        self.detector.setInput(imageBlob)
+        detections = self.detector.forward()
 
-                # ensure that the detection with the largest probability also
-                # means our minimum probability test (thus helping filter out
-                # weak detections)
-                if confidence > self.confidence:
-                    # compute the (x, y)-coordinates of the bounding box for
-                    # the face
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
+        # ensure at least one face was found
+        if len(detections) > 0:
+            # we're making the assumption that each image has only ONE
+            # face, so find the bounding box with the largest probability
+            i = np.argmax(detections[0, 0, :, 2])
+            confidence = detections[0, 0, i, 2]
 
-                    # extract the face ROI and grab the ROI dimensions
-                    face = image[startY:endY, startX:endX]
-                    (fH, fW) = face.shape[:2]
+            # ensure that the detection with the largest probability also
+            # means our minimum probability test (thus helping filter out
+            # weak detections)
+            if confidence > self.confidence:
+                # compute the (x, y)-coordinates of the bounding box for
+                # the face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-                    # ensure the face width and height are sufficiently large
-                    if fW < 20 or fH < 20:
-                        continue
+                # extract the face ROI and grab the ROI dimensions
+                face = image[startY:endY, startX:endX]
+                (fH, fW) = face.shape[:2]
 
-                    # construct a blob for the face ROI, then pass the blob
-                    # through our face embedding model to obtain the 128-d
-                    # quantification of the face
-                    faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-                                                     (96, 96), (0, 0, 0), swapRB=True, crop=False)
-                    self.embedder.setInput(faceBlob)
-                    vec = self.embedder.forward()
+                # ensure the face width and height are sufficiently large
+                if fW < 20 or fH < 20:
+                    return
 
-                    # add the name of the person + corresponding face
-                    # embedding to their respective lists
-                    # TODO: LabelEncoder fit transform & recognizer fit embeddings
-                    pQueue \
-                        .setdefault(matricCode, []) \
-                        .append(vec.flatten())
+                # construct a blob for the face ROI, then pass the blob
+                # through our face embedding model to obtain the 128-d
+                # quantification of the face
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                                                 (96, 96), (0, 0, 0), swapRB=True, crop=False)
+                self.embedder.setInput(faceBlob)
+                vec = self.embedder.forward()
+
+                # add the name of the person + corresponding face
+                # embedding to their respective lists
+                # TODO: LabelEncoder fit transform & recognizer fit embeddings
+                pQueue \
+                    .setdefault(matricCode, []) \
+                    .append(vec.flatten())
 
     def quantifyFaces(self):
         # this is expensive, try to limit calls as much as possible
