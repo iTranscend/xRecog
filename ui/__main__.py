271f2f50becf413d0e956d3967584ba464fb3e75
Miraclx <omiraculous@gmail.com>
2020-08-28 22:47:20 +0100

    Extend dispatcher throughout __main__.py for loading courses and students

--- ui/__main__.py
+++ ui/__main__.py
@@ -18,87 +18,91 @@ def mountTestInstance(main_window):
     args = sys.argv[1:]
     num_students = int(args[0]) if len(args) else 10000
 
-    courses = [
-        "Computer Science",
-        "Physics",
-        "Chemistry",
-        "Law",
-        "Sociology",
-        "Political Sciences",
-        "Art",
-        "Philosophy",
-        "Music",
-        "Anthropology",
-        "Psychology",
-        "English",
-        "Astrophysics",
-        "Biology",
-        "Geography",
-        "Physiotheraphy",
-        "Medicine",
-        "French",
-        "Statistics",
-        "Biochemistry",
-        "Cybersecurity",
-        "Criminology",
-        "Economics",
-        "Epidemiology",
-        "Statistics",
-        "Mathematics"
-    ]
-    with main_window.logr(
-            "Loading %d course%s" % (len(courses), "" if len(courses) == 1 else 's'), force=True):
-        main_window.loadCourses(courses)
+    def loadCoursesAndStudents(logTick):
+        logTick("Preparing courses...", 1)
+        courses = [
+            "Computer Science",
+            "Physics",
+            "Chemistry",
+            "Law",
+            "Sociology",
+            "Political Sciences",
+            "Art",
+            "Philosophy",
+            "Music",
+            "Anthropology",
+            "Psychology",
+            "English",
+            "Astrophysics",
+            "Biology",
+            "Geography",
+            "Physiotheraphy",
+            "Medicine",
+            "French",
+            "Statistics",
+            "Biochemistry",
+            "Cybersecurity",
+            "Criminology",
+            "Economics",
+            "Epidemiology",
+            "Statistics",
+            "Mathematics"
+        ]
+        with main_window.logr(
+                "Loading %d course%s" % (len(courses), "" if len(courses) == 1 else 's'), force=True):
+            logTick("Loading courses into UI...", 10)
+            main_window.loadCourses(courses)
 
-    max_students = max(10000, num_students)
+        max_students = max(10000, num_students)
 
-    with main_window.logr("Generating %d student%s" % (num_students, "" if num_students == 1 else 's'), force=True):
-        faker = Faker()
-        students = []
-        pad = len(str(num_students))
-        matric_numbers = random.sample(range(0, max_students), num_students)
+        logTick("Generating decoy students...", 5)
+        with main_window.logr("Generating %d student%s" % (num_students, "" if num_students == 1 else 's'), force=True):
+            faker = Faker()
+            students = []
+            pad = len(str(num_students))
+            matric_numbers = random.sample(
+                range(0, max_students), num_students)
 
-        def newStudent(matric_number):
-            male = bool(random.getrandbits(1))
-            students.append({
-                "firstName": faker.first_name_male() if male else faker.first_name_female(),
-                "middleName": faker.first_name_male() if male else faker.first_name_female(),
-                "lastName": faker.last_name_male() if male else faker.last_name_female(),
-                "entryYear": random.randint(MIN_YEAR, MAX_YEAR + 1),
-                "matriculationCode": f"%0{pad}d" % matric_number,
-                "courseOfStudy": random.randint(0, len(courses) - 1),
-                "markPresent": False
-            })
-        studentJobs = Parallelizer(matric_numbers, min(
-            num_students, 100 if num_students >= 80000 else 8), newStudent)
-        studentJobs.start()
-        studentJobs.joinAll()
+            def newStudent(matric_number):
+                logTick("Generating decoy students [%d/%d]..." % (
+                    len(students) + 1, num_students), tick=45 / num_students)
+                male = bool(random.getrandbits(1))
+                students.append({
+                    "firstName": faker.first_name_male() if male else faker.first_name_female(),
+                    "middleName": faker.first_name_male() if male else faker.first_name_female(),
+                    "lastName": faker.last_name_male() if male else faker.last_name_female(),
+                    "entryYear": random.randint(MIN_YEAR, MAX_YEAR + 1),
+                    "matriculationCode": f"%0{pad}d" % matric_number,
+                    "courseOfStudy": random.randint(0, len(courses) - 1),
+                    "markPresent": False
+                })
+            studentJobs = Parallelizer(matric_numbers, min(
+                num_students, 100 if num_students >= 80000 else 8), newStudent)
+            studentJobs.start()
+            studentJobs.joinAll()
 
-    with main_window.logr(
-        "Populating UI with %d student%s" % (
-            len(students), "" if len(students) == 1 else 's'),
-        "Populated UI with %d student%s" % (
-            len(students), "" if len(students) == 1 else 's'),
-        reenter=True, force=True, is_async=True
-    ) as logr:
-        def loadStudents(logTick):
-            nStudents = len(students)
+        with main_window.logr(
+            "Populating UI with %d student%s" % (
+                len(students), "" if len(students) == 1 else 's'),
+            "Populated UI with %d student%s" % (
+                len(students), "" if len(students) == 1 else 's'),
+            reenter=True, force=True
+        ) as logr:
             for (index, job) in enumerate(main_window.loadStudents(students)):
                 logTick(
-                    f"Loading student into UI [%d/%d]..." % (index + 1, nStudents), tick=(100 / nStudents))
+                    f"Loading students into UI [%d/%d]..." % (index + 1, num_students), tick=(40 / num_students))
                 job.wait()
-            logr.done()
 
-        main_window._dispatch(
-            loadStudents,
-            max=100, timeout=0,
-            title="Loading Students",
-            message="Loading Students, please wait..."
-        )
+        main_window.setAboutText(
+            "xRecog\n\nApp Description\n\n2020 (c) Femi Bankole, Miraculous Owonubi")
+        main_window.setRegistrationYearRange(MIN_YEAR, MAX_YEAR)
 
-    main_window.setAboutText(
-        "xRecog\n\nApp Description\n\n2020 (c) Femi Bankole, Miraculous Owonubi")
-    main_window.setRegistrationYearRange(MIN_YEAR, MAX_YEAR)
+    main_window._dispatch(
+        loadCoursesAndStudents,
+        max=100, timeout=0,
+        title="Loading demo instance...",
+        message="Loading courses and students, please wait..."
+    )
 
     def handleTestRegData(data):
         main_window.loadStudent(data)
